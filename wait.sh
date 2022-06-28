102ef45b9ec3fa520547604a45db4e78acb9f6ff
Artem Chebotko <artemchebotko@gmail.com>
2022-06-28 13:48:43 -0500

    Update wait.sh

--- wait.sh
+++ wait.sh
@@ -6,9 +6,9 @@ wget -q https://downloads.datastax.com/dsbulk/dsbulk.tar.gz
 tar -xzf dsbulk.tar.gz
 rm dsbulk.tar.gz
 mv dsbulk* dsbulk
-PATH=$PATH:/workspace/music-data/dsbulk/bin >$(tty)
+#PATH=$PATH:/workspace/music-data/dsbulk/bin
 echo 'PATH="$PATH:/workspace/music-data/dsbulk/bin"' >> ~/.bashrc
-
+source ~/.bashrc
 
 echo -n 'Waiting for Cassandra to start...'
 timeout 60 bash -c 'until cqlsh -e "describe cluster" >/dev/null 2>&1; do sleep 1; echo -n "."; done'
